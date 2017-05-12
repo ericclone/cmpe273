7e6f4962494e74828723a301820257b73725ebf1
ericclone <eric.clone@gmail.com>
2017-05-12 13:16:23 -0700

    Make post more tolerant

--- assignment2/menu_post.py
+++ assignment2/menu_post.py
@@ -5,13 +5,5 @@ def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('Menus')
     
-    response = table.put_item(
-        Item={
-            "menu_id":event['menu_id'],
-            "selection":event['selection'],
-            "size":event['size'],
-            "price":event['price'],
-            "store_hours":event['store_hours']
-        }
-    )
+    response = table.put_item(Item=event)
     return 'OK'
